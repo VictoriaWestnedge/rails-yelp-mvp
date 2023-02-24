93a59c0f4d54fb15631cb6f951f221cc9e34a057
Victoria Westnedge victoriawestnedge@gmail.com <victoriawestnedge@gmail.com>
2023-02-23 21:52:54 -0300

    Rails-Yelp-MVP completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants do
+    resources :reviews, only: [:index, :create] # no tiene ID todavía
+  end
+    # resources :reviews, only: [:show] # ya tiene ID
+
+  # post "restaurants/:id/reviews", to: "reviews#index"
+  # get "restaurants/:id/reviews/new", to: "reviews#new"
+
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/new", to: "restaurants#new"
+
+  # get "restaurants/:id", to: "restaurants#show"
+  # get "restaurants", to: "restaurants#index"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all
+Restaurant.create({ name: "Ikoyi", address: "180 Strand, Temple, London WC2R 1EA", phone_number: "+44 20 3583 4660", category: "japanese"})
+Restaurant.create({ name: "Berners Tavern", address: "10 Berners St, London W1T 3NP", phone_number: "+44 20 7908 7979", category: "belgian"})
+Restaurant.create({ name: "Copita", address: "27 D'Arblay St, London W1F 8EN", phone_number: "+44 20 8444 0220", category: "italian"})
+Restaurant.create({ name: "Murano", address: "20 Queen St, London W1J 5PP", phone_number: "+44 20 7030 4360", category: "chinese"})
+Restaurant.create({ name: "The Pem", address: "22-28 Broadway, London SW1H 0BH", phone_number: "+44 20 3365 1360", category: "french"})
